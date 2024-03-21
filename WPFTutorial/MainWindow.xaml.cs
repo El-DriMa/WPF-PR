4bdea3ebb78e30e92102484c35b0fe6f3ba0a79f
Amna Hodžić <ahodzic649@outlook.com>
2024-03-21 21:03:03 +0100

    btn test

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -2,14 +2,19 @@
 
 namespace WPFTutorial
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+  
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            tbHello.Text = "Hello World";
+            btnRun.Content = "Stop";
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
