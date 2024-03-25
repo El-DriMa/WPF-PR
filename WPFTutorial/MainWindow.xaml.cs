b792ecdb5c37cd3db3ab42c6e65fdebba7c88187
Amna Hodžić <ahodzic649@outlook.com>
2024-03-25 14:38:39 +0100

    data bindings

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -1,15 +1,43 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.DataContracts;
+using System.Windows;
 
 namespace WPFTutorial
 {
   
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         //bool running = false;
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
            
+        }
+
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        private string boundText;
+        public string BoundText   
+        {
+            get { return boundText; }
+            set { 
+                boundText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "set from code";
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         /*private void btnToggleRun_Click(object sender, RoutedEventArgs e)
