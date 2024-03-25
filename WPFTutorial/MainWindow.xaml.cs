db1bec4b6e553ff3813d4bab12484ae818895c10
Amna Hodžić <ahodzic649@outlook.com>
2024-03-25 17:20:39 +0100

    file dialog

--- WPFTutorial/MainWindow.xaml.cs
+++ WPFTutorial/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.Win32;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization.DataContracts;
 using System.Windows;
@@ -6,39 +7,58 @@ using System.Windows;
 namespace WPFTutorial
 {
   
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         //bool running = false;
         public MainWindow()
         {
-            DataContext = this;
+           // DataContext = this;
             InitializeComponent();
            
         }
 
-        
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-
-        private string boundText;
-        public string BoundText   
+        private void btnFire_Click(object sender, RoutedEventArgs e)
         {
-            get { return boundText; }
-            set { 
-                boundText = value;
-                OnPropertyChanged();
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "C# Source Files | *.cs";//Sve sto zavrsava sa .cs
+         // fd.InitialDirectory = "C:\\temp"; -ako folder ne postoji,otvori na zadnjem
+            fd.Title = "Pweaseeeeee";
+            fd.Multiselect = true;
+
+            //?-3 states,null,true false,zato treba == true
+            bool? success= fd.ShowDialog();
+            if (success==true)
+            {
+                //[] kolekcija vise fajlova
+                string path = fd.FileName;//citava putanja
+                string fileName = fd.SafeFileName;//samo ime fajla
+                tbInfo.Text = fileName;
             }
         }
 
-        private void btnSet_Click(object sender, RoutedEventArgs e)
-        {
-            BoundText = "set from code";
-        }
 
-        private void OnPropertyChanged([CallerMemberName] string propertyName=null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        /* public event PropertyChangedEventHandler? PropertyChanged;
+
+
+         private string boundText;
+         public string BoundText   
+         {
+             get { return boundText; }
+             set { 
+                 boundText = value;
+                 OnPropertyChanged();
+             }
+         }
+
+         private void btnSet_Click(object sender, RoutedEventArgs e)
+         {
+             BoundText = "set from code";
+         }
+
+         private void OnPropertyChanged([CallerMemberName] string propertyName=null)
+         {
+             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+         }*/
 
         /*private void btnToggleRun_Click(object sender, RoutedEventArgs e)
         {
